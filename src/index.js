660279d1e6fb60c931f05b299d592feb216f7ffd
Bruno Agutoli <bruno.agutoli@gmail.com>
2019-02-19 17:20:00 -0300

    log improvements

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ function BabelPluginSimpleI18N(babel) {
           const { functionName = "__", outputFile = false } = state.opts
           if (path.node.callee.name === state.opts.functionName && t.isIdentifier(path.node.callee)) {
             if(path.node.arguments.length >= 1) {
+              const currentFile = state.filename.replace(`${process.env.PWD}/`, '');
               const importName = addNamed(path, 'FormattedMessage', 'react-intl');
               if (path.node.arguments[0].type == 'StringLiteral') {
                 // const id = (path.node.arguments[0]||{}).value
@@ -41,7 +42,7 @@ function BabelPluginSimpleI18N(babel) {
                 const fileOutput = resolve(join(process.cwd(), state.opts.outputFile))
 
                 if (process.env.BABEL_ENV !== 'production') {
-                  console.warn(`Warning: babel-plugin-react-intl-export-keys mod dev.`)
+                  console.warn(`- [I18n][${currentFile}]: ${defaultMessage}`)
                 }
 
                 if (process.env.BABEL_ENV === 'production') {
